4bb1c0801373e6f3ad0d4ea36c2831808d596d45
Willians Arrezendes <4rrezende5@gmail.com>
2024-09-21 15:55:36 -0300

    Desafio Target

---
+++ ex1.js
@@ -0,0 +1,15 @@
+
+const index = 13
+var sum = 0
+var k = 0
+
+while (k < index) {
+    k = k + 1
+    sum = sum + k
+}
+
+console.log(sum);
+
+// O valor da variável soma será 91. Isso porque a cada repetição do laço while, a variável k
+// terá +1 adicionado ao seu valor, e a variável soma terá o novo valor de k adicionado ao seu valor
+// atual.
---
+++ ex2.js
@@ -0,0 +1,12 @@
+
+const number = 12
+var fibonacci = [0, 1]
+var index = 0
+
+while (number > fibonacci[fibonacci.length-1]) {
+    fibonacci.push(fibonacci[index]+fibonacci[index+1])
+    indice+=1
+}
+
+console.log(fibonacci)
+console.log(fibonacci.includes(number)? true : false)
