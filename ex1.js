948cdb4f2fb08e9388b795d6bf8ea4734f13de2c
4rrezende5 <4rrezende5@gmail.com>
2024-09-22 12:40:46 -0300

    Correção de dados

--- ex1.js
+++ ex1.js
@@ -10,6 +10,7 @@ while (k < index) {
 
 console.log(sum);
 
-// O valor da variável soma será 91. Isso porque a cada repetição do laço while, a variável k
-// terá +1 adicionado ao seu valor, e a variável soma terá o novo valor de k adicionado ao seu valor
-// atual.
+/* O valor da variável soma será 91. Isso porque a cada repetição do laço while, a variável k
+ terá +1 adicionado ao seu valor, e a variável soma terá o novo valor de k adicionado ao seu valor
+ atual.
+ */
